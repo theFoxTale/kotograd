54546c4ef8e771bccd1c66f042cf4c78783d881a
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-06-10 08:08:24 +0300

    chore: update css

--- src/components/CitizenCard/CitizenCard.tsx
+++ src/components/CitizenCard/CitizenCard.tsx
@@ -1,6 +1,9 @@
 import { formFields } from '../../constants/formText';
 
 import './CitizenCard.css';
+import genderIcon from '../../assets/images/icons/gender.png';
+import ownerIcon from '../../assets/images/icons/owner.png';
+import ageIcon from '../../assets/images/icons/age.png';
 
 interface CitizenCardProps {
   id: string;
@@ -24,15 +27,35 @@ export const CitizenCard = ({
     <div className={`citizen-card ${isHighlighted ? 'highlight' : ''}`}>
       <div className="citizen-avatar">🐱</div>
       <div className="citizen-info">
-        <p>
-          <strong>{name}</strong>, {age} лет
+        <p className="citizen-header">
+          <strong>{name}</strong>
         </p>
-        <p>{email}</p>
-        <p>
-          Пол:{' '}
-          {gender === 'male' ? formFields.genderMale : formFields.genderFemale}
-        </p>
-        <p className="citizen-date">{new Date(createdAt).toLocaleString()}</p>
+
+        <div className="citizen-card__details">
+          <div className="detail-item">
+            <img src={genderIcon} alt="Пол" className="card-icon" />
+            <p className="citizen-text">
+              {gender === 'male'
+                ? formFields.genderMale
+                : formFields.genderFemale}
+            </p>
+          </div>
+
+          <div className="detail-item">
+            <img src={ageIcon} alt="Пол" className="card-icon" />
+            <p className="citizen-text">{age} лет</p>
+          </div>
+
+          <div className="detail-item">
+            <img src={ownerIcon} alt="Пол" className="card-icon" />
+            <p className="citizen-text">{email}</p>
+          </div>
+        </div>
+
+        <div className="citizen-card__footer">
+          <p className="citizen-date">Присоединился к нам </p>
+          <p className="citizen-date">{new Date(createdAt).toLocaleString()}</p>
+        </div>
       </div>
     </div>
   );
